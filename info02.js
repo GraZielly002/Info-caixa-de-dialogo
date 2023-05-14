63252dc7e640d347c19aab9c97523c279ffe667b
Grazielly <104026642+GraZielly002@users.noreply.github.com>
2023-05-14 19:00:36 -0300

    Add files via upload

---
+++ info02.js
@@ -0,0 +1,7 @@
+var nome = window.prompt(`Digite seu nome completo ${nome}`)
+        var numero = Number(window.prompt(`Digite seu numero ${numero}`))
+        var genero = window.prompt(`Digite o seu gênero ${genero}`)
+
+        document.write(`Nome: ${nome.toUpperCase()}</br>`)
+        document.write(`Seu número: ${numero}</br>`)
+        document.write(`Gênero: ${genero.toUpperCase()}`)
